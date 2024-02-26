39f832efe06726681a5a7e4e7c6effe40f2d1418
Ethanw1027 <103462549+Ethanw1027@users.noreply.github.com>
2024-02-26 16:55:34 -0500

    Netlify attempt 3

--- app.py
+++ app.py
@@ -138,4 +138,8 @@ def prof_search():
     #    return render_template('prof_search.html', ranked_profs=None)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    # For testing
+    # app.run(debug=True)
+    
+    # For Netlify deployment
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
